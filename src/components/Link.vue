a40fbfd21b66f00ad3ae6f785846c91b028f21ec
Matthieu Dartiguenave <matthieu@localhost.localdomain>
2020-04-12 07:27:35 +0100

    Link animation issue

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -11,28 +11,31 @@ import Theme from "./Theme.vue"
 @Component
 export default class Link extends Mixins(Theme) {
   @Prop() private link!: LinkModel
-  private tweenedTarget = OriginPoint()
+  private tweenedLink = { origin: OriginPoint(), target: OriginPoint() }
 
   mounted() {
     gsap.fromTo(
-      this.tweenedTarget,
+      this.tweenedLink,
       this.animationSpeed,
-      this.link.origin,
-      this.link.target
+      { origin: this.link.origin, target: this.link.origin },
+      { origin: this.link.origin, target: this.link.target }
     )
   }
 
   @Watch("link", { deep: true })
   onPointModelChanged(value: LinkModel, oldValue: LinkModel) {
-    gsap.to(this.tweenedTarget, this.animationSpeed, value.target)
+    gsap.to(this.tweenedLink, this.animationSpeed, {
+      origin: value.origin,
+      target: value.target
+    })
   }
 
   private get path() {
-    const ox = this.link.origin.x + 50
-    const oy = this.link.origin.y
+    const ox = this.tweenedLink.origin.x + 50
+    const oy = this.tweenedLink.origin.y
 
-    const tx = this.tweenedTarget.x
-    const ty = this.tweenedTarget.y
+    const tx = this.tweenedLink.target.x
+    const ty = this.tweenedLink.target.y
 
     return `M${ox},${oy}C${(ox + tx) * 0.5},${oy},${ox},${ty},${tx},${ty}`
   }
--- src/components/Theme.vue
+++ src/components/Theme.vue
@@ -4,7 +4,7 @@ import { Component, Prop, Vue, Watch } from "vue-property-decorator"
 @Component
 export default class Theme extends Vue {
   get animationSpeed() {
-    return 0.3
+    return 1
   }
 }
 </script>
